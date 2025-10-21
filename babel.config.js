448aabc40e04419553fc5cbe6e1a75b2ae26de88
Nguyễn Đức Thông <43933486+DianaSensei@users.noreply.github.com>
2025-10-21 21:35:07 +0700

    fix

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,24 @@
-module.exports = {
-  "plugins": [
-    [
-      "module-resolver",
-      {
-        "root": [
-          "./"
-        ],
-        "alias": {
-          "@/*": "./*",
-          "@/src/*": "./src/*",
-          "@/modules/*": "./src/modules/*",
-          "@/shared/*": "./src/shared/*",
-          "@/core/*": "./src/core/*"
-        }
-      }
-    ]
-  ]
-}
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@/*": "./*",
+            "@/src/*": "./src/*",
+            "@/modules/*": "./src/modules/*",
+            "@/shared/*": "./src/shared/*",
+            "@/core/*": "./src/core/*",
+          },
+        },
+      ],
+      // react-native-reanimated plugin must be last
+      "react-native-reanimated/plugin",
+    ],
+  };
+};
---
+++ index.js
@@ -0,0 +1,6 @@
+// Initialize gesture handler and reanimated before any other imports
+import "react-native-gesture-handler";
+import "react-native-reanimated";
+
+// Export expo-router entry
+export { default } from "expo-router/entry";
